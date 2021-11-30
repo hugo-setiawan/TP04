9165982844453c55c3dfa972401872e9817aa1aa
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 19:58:09 +0700

    Add self to widget declaration in GUI class init

--- barcode_generator.py
+++ barcode_generator.py
@@ -4,12 +4,12 @@ from tkinter.font import Font
 class GUI:
     def __init__(self, root: tk.Tk):
         self.root = root
-        label_font = Font(family="Helvetica", weight="bold")
-        save_label = tk.Label(text="Save barcode to PS file [eg: EAN13.eps]:", font=label_font)
-        save_label.pack()
+        self.label_font = Font(family="Helvetica", weight="bold")
+        self.save_label = tk.Label(text="Save barcode to PS file [eg: EAN13.eps]:", font=label_font)
+        self.save_label.pack()
         # INSERT TEXTBOX FOR FILENAME HERE
-        code_label = tk.Label(text="Enter code (first 12 decimal digits):", font=label_font)
-        code_label.pack()
+        self.code_label = tk.Label(text="Enter code (first 12 decimal digits):", font=label_font)
+        self.code_label.pack()
         # INSERT TEXTBOX FOR FILENAME HERE
         # INSERT CANVAS HERE
 
