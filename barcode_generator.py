3a5605398655172408cd7078a4729f26695ba529
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 20:00:09 +0700

    Add root to each Label

--- barcode_generator.py
+++ barcode_generator.py
@@ -5,10 +5,10 @@ class GUI:
     def __init__(self, root: tk.Tk):
         self.root = root
         self.label_font = Font(family="Helvetica", weight="bold")
-        self.save_label = tk.Label(text="Save barcode to PS file [eg: EAN13.eps]:", font=label_font)
+        self.save_label = tk.Label(root,text="Save barcode to PS file [eg: EAN13.eps]:", font=self.label_font)
         self.save_label.pack()
         # INSERT TEXTBOX FOR FILENAME HERE
-        self.code_label = tk.Label(text="Enter code (first 12 decimal digits):", font=label_font)
+        self.code_label = tk.Label(root,text="Enter code (first 12 decimal digits):", font=self.label_font)
         self.code_label.pack()
         # INSERT TEXTBOX FOR FILENAME HERE
         # INSERT CANVAS HERE
