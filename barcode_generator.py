1ccc3697eef636fc69a7b71ac08bc963d0b314d2
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 20:01:13 +0700

    Implement textbox for filename

--- barcode_generator.py
+++ barcode_generator.py
@@ -7,7 +7,9 @@ class GUI:
         self.label_font = Font(family="Helvetica", weight="bold")
         self.save_label = tk.Label(root,text="Save barcode to PS file [eg: EAN13.eps]:", font=self.label_font)
         self.save_label.pack()
-        # INSERT TEXTBOX FOR FILENAME HERE
+        self.filename = tk.StringVar()
+        self.filename_field = tk.Entry(root, textvariable=self.filename)
+        self.filename_field.pack()
         self.code_label = tk.Label(root,text="Enter code (first 12 decimal digits):", font=self.label_font)
         self.code_label.pack()
         # INSERT TEXTBOX FOR FILENAME HERE
