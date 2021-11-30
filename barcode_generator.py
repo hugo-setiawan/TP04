9cdad116afcbee7277090640d1e09f82137a9636
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 20:22:14 +0700

    Put title as implicit of barcode_gui

--- barcode_generator.py
+++ barcode_generator.py
@@ -2,7 +2,10 @@ import tkinter as tk
 from tkinter.font import Font
 
 class barcode_gui:
+    def __init__(self, root: tk.Tk):
         self.root = root
+        self.title = "EAN-13 Barcode Generator"
+        self.root.title(self.title)
         self.label_font = Font(family="Helvetica", weight="bold")
         self.save_label = tk.Label(root,text="Save barcode to PS file [eg: EAN13.eps]:", font=self.label_font)
         self.save_label.pack()
@@ -22,7 +25,7 @@ class barcode_gui:
 
 def main():
     root = tk.Tk()
-    window = barcode_gui(root, "EAN-13 Barcode Generator")
+    window = barcode_gui(root)
     window.mainloop()
 
 if __name__ == "__main__":
