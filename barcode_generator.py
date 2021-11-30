9347be0fe541c213dd42cc103bd40f42429e8bb1
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 21:58:56 +0700

    refactor getting first digit

--- barcode_generator.py
+++ barcode_generator.py
@@ -38,9 +38,10 @@ class barcode_canvas(tk.Canvas):
 
     def encode(self):
         first_seq = self.code[1:7]
-        first_group = self.FIRST_STRUCTURE[int(self.code[0])]
+        first_digit = int(self.code[0])
+        first_group = self.FIRST_STRUCTURE[first_digit]
         second_seq = self.code[7:]
-        second_group = self.SECOND_STRUCTURE[int(self.code[0])]
+        second_group = self.SECOND_STRUCTURE[first_digit]
         encoded = ""
         encoded += "101"
         # FIRST SEQUENCE
