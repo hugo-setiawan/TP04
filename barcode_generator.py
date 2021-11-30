7b352e1f46154b7aaa3cfdaad6b5e3d25a6068a2
hugo-setiawan <hugo.sulaiman@ui.ac.id>
2021-11-30 20:18:03 +0700

    rename GUI class

--- barcode_generator.py
+++ barcode_generator.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter.font import Font
 
-class GUI:
-    def __init__(self, root: tk.Tk):
+class barcode_gui:
         self.root = root
         self.label_font = Font(family="Helvetica", weight="bold")
         self.save_label = tk.Label(root,text="Save barcode to PS file [eg: EAN13.eps]:", font=self.label_font)
@@ -23,7 +22,7 @@ class GUI:
 
 def main():
     root = tk.Tk()
-    window = GUI(root)
+    window = barcode_gui(root, "EAN-13 Barcode Generator")
     window.mainloop()
 
 if __name__ == "__main__":
